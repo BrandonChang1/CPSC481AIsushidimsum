2bd46f1a10e3829703287824f4b39b15c378faee
Brandon Chang <brandon_chang@csu.fullerton.edu>
2024-11-28 07:52:28 -0800

    fixed distances problem

--- chopstick.gd
+++ chopstick.gd
@@ -1,6 +1,6 @@
 extends RichTextLabel
 
-
+var chopsticks_image = preload("res://images/chopsticks.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
